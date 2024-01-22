87996acd70333cdb7718fb3c8f2bd9a341929610
Bimisha Mishra <bimu.umib147@gmail.com>
2024-01-22 23:09:51 +0545

    feat: added type.js to animate the text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./Home.css";
+import Typed from "typed.js";
+//import profileImage from "./path/to/your/image.jpg";  // Replace with the path to your image
 
-const Home =() => {
-    return(
-        <div>
-            Home Section
-        </div>
-    )
-}
+const Home = () => {
+  useEffect(() => {
+    // Initialize Typed.js
+    const options = {
+      strings: ["Full Stack Developer", "UI/UX designer"],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+      showCursor: false,
+      //cursorChar: "|"
+    };
+
+    const typed = new Typed(".typed-text", options);
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="home-container">
+      <div className="text-container">
+        <h1>Hi, my name is Bimisha Mishra</h1>
+        <h3 className="typed-text"></h3>
+      </div>
+      {/* <div className="image-container">
+        <img src={profileImage} alt="Profile" />
+      </div> */}
+    </div>
+  );
+};
 
 export default Home;
